b22145d0883b3291b6cfbce4615c893d5418b94c
Faraaz Ali Khan <itsfaraazak@gmail.com>
2024-03-08 21:30:43 +0530

    flask url

--- client/src/lib/gatepassrAPI.ts
+++ client/src/lib/gatepassrAPI.ts
@@ -1,6 +1,5 @@
-import { env } from '$env/dynamic/public'
-console.log(env.PUBLIC_GATEPASSAPI_URL)
-export const gatepassrAPI = env.PUBLIC_GATEPASSAPI_URL ?? "http://127.0.0.1:5000"
+
+export const gatepassrAPI = "https://gatepassrapi.azurewebsites.net"
 
 //"https://gatepassrapi.azurewebsites.net"
 //"http://127.0.0.1:5000";

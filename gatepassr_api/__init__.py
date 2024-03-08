ccd84caa1a39a3a2e599cc7f1f3540b23f471e71
itsfaraazak <154220509+itsfaraazak@users.noreply.github.com>
2024-03-08 07:32:54 +0530

    Update __init__.py
    testing the module not found error

--- gatepassr_api/__init__.py
+++ gatepassr_api/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask, session, redirect, render_template, request, url_for
 #import identity.web
 #import requests
 
-from flask_session import Session
+#from flask_session import Session
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity

7fd67043f168ccc09df306b924639120fdde05ba
Faraaz Ali Khan <itsfaraazak@gmail.com>
2024-03-08 19:36:23 +0530

    added API URL

--- client/src/lib/gatepassrAPI.ts
+++ client/src/lib/gatepassrAPI.ts
@@ -1,1 +1,5 @@
-export const gatepassrAPI = "http://127.0.0.1:5000";
+
+export const gatepassrAPI = "https://gatepassrapi.azurewebsites.net"
+
+//"https://gatepassrapi.azurewebsites.net"
+//"http://127.0.0.1:5000";
